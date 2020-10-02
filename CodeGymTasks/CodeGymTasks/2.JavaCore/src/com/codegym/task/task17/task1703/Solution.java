08e58831731fad0c6229218cddb595c717dd13b7
Collin Morudi <realcollin112@gmail.com>
2020-10-02 12:42:24 +0200

    Completed the synchronizes notes task

--- CodeGymTasks/CodeGymTasks/2.JavaCore/src/com/codegym/task/task17/task1703/Solution.java
+++ CodeGymTasks/CodeGymTasks/2.JavaCore/src/com/codegym/task/task17/task1703/Solution.java
@@ -20,13 +20,20 @@ public class Solution {
 
         public void addNote(int index, String note) {
             System.out.println("A note [" + note + "] will now be added at position " + index);
-            notes.add(index, note);
+
+            synchronized (notes) {
+                notes.add(index, note);
+            }
+
             System.out.println("The note [" + note + "] has already been added");
         }
 
         public void removeNote(int index) {
             System.out.println("A note will now be deleted from position " + index);
-            String note = notes.remove(index);
+            String note;
+            synchronized (notes) {
+                 note = notes.remove(index);
+            }
             System.out.println("The note [" + note + "] has already been deleted from position " + index);
         }
     }
