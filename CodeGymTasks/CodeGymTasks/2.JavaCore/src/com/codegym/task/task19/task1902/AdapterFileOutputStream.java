b1bb00fbcb1656d7fa48ead89069f1bb5e491ec5
Collin Morudi <realcollin112@gmail.com>
2020-10-03 17:12:51 +0200

    Solved the Adapter Task

--- CodeGymTasks/CodeGymTasks/2.JavaCore/src/com/codegym/task/task19/task1902/AdapterFileOutputStream.java
+++ CodeGymTasks/CodeGymTasks/2.JavaCore/src/com/codegym/task/task19/task1902/AdapterFileOutputStream.java
@@ -5,12 +5,38 @@ Adapter
 
 */
 
-public class AdapterFileOutputStream {
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-    public static void main(String[] args) {
+public class AdapterFileOutputStream implements AmigoStringWriter {
 
+    private FileOutputStream fileOutputStream;
+
+    public AdapterFileOutputStream(FileOutputStream fileOutputStream) {
+        this.fileOutputStream = fileOutputStream;
+    }
+
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream("Data.txt");
+        AmigoStringWriter amigoStringWriter = new AdapterFileOutputStream(fileOutputStream);
+        amigoStringWriter.writeString("Hello Collin\r\nHow you doing man?");
     }
 
 
+    @Override
+    public void flush() throws IOException {
+        this.fileOutputStream.flush();
+    }
+
+    @Override
+    public void writeString(String s) throws IOException {
+        byte[] bytes = s.getBytes();
+        this.fileOutputStream.write(bytes);
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.fileOutputStream.close();
+    }
 }
 
